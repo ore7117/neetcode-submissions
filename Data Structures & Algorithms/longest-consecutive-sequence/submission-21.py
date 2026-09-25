d321f9e18097806f1f820a3e9117d6f1f4c2fb34
Ore Ogundipe <37311965+ore7117@users.noreply.github.com>
2026-09-25 14:31:04 -0400

    Add: longest-consecutive-sequence - submission-21

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-21.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        numSet = set(nums)
+
+        count =1 
+        longest=1 
+        if not nums:
+            return 0
+
+        for n in nums:
+            #start of streak
+            if n-1 not in numSet: 
+                count =1 
+            
+                while(n+count) in numSet:
+                    count +=1 
+
+            longest = max(count,longest)
+        
+        return longest
+
+                
